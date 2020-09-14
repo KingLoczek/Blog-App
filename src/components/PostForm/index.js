fdee6817730c92b562048c00a15986627b2a38df
Konrad <KingLoczek@users.noreply.github.com>
2020-09-14 15:51:04 +0200

    addPost

---
+++ src/components/PostForm/index.js
@@ -0,0 +1,73 @@
+import React, {useState} from 'react';
+import "./style.css";
+
+
+ function PostForm() {
+    const Post = ({ title, author, text, }) => (
+        <div>
+          <p className="postTitle">{title}</p>
+          <p className="postedBy">Posted by {author} </p>
+          <p className="postText">{text}</p>
+          <hr></hr>
+        </div>
+      );
+    const [posts, setPosts] = useState([]);
+
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [text, setText] = useState("");
+    
+  
+    const addPost = (post) => {
+      setPosts([post, ...posts]);
+    };
+  
+    const onAddButtonClick = () => {
+      const post = {title, author, text };
+      addPost(post);
+    };
+  
+    return (
+      <div>
+        {posts.length === 0 && <p>No posts.</p>}
+        {posts.map((post) => (
+          <Post title={post.title} author={post.author} text={post.text} />
+        ))}
+        <div>
+          <p class="add">Add a new post</p>
+          <p>
+          <input
+              type="text"
+              placeholder="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            </p>
+            <p>
+            <input
+              type="text"
+              placeholder="Author"
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+            />
+          </p>
+          <p>
+            <input
+              type="text"
+              placeholder="Text"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+            
+          </p>
+          <p>
+            <button onClick={onAddButtonClick}>Add</button>
+          </p>
+        </div>
+      </div>
+    );
+ }
+ 
+ export default PostForm
+ 
+
