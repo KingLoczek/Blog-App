3849cdb21e8a23a1826d346ba991387dd0cfd6fa
Konrad <KingLoczek@users.noreply.github.com>
2021-05-22 00:08:47 +0200

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-This is my Blog app. Currently part of the frontend is added, which is improving all the time. Later I will gradually add the backend.
---
+++ precache-manifest.90282ad4c6a3d7bab9e4c0a103f30d19.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177ae9588ea18d9bbfc5b04f587e65c0",
+    "url": "/Blog-App/index.html"
+  },
+  {
+    "revision": "ce53f72eab8deb3e6885",
+    "url": "/Blog-App/static/css/main.30f9880d.chunk.css"
+  },
+  {
+    "revision": "598766e474c684470c7d",
+    "url": "/Blog-App/static/js/2.420f9828.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Blog-App/static/js/2.420f9828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce53f72eab8deb3e6885",
+    "url": "/Blog-App/static/js/main.8852775b.chunk.js"
+  },
+  {
+    "revision": "7709aec9352a722c9feb",
+    "url": "/Blog-App/static/js/runtime-main.579ce824.js"
+  },
+  {
+    "revision": "d6054ba3f0c2dbee858b554aa6172f03",
+    "url": "/Blog-App/static/media/pin.d6054ba3.png"
+  },
+  {
+    "revision": "18b95dec6d61e7aa34f4eca12d4a7d7a",
+    "url": "/Blog-App/static/media/searchIcon.18b95dec.png"
+  },
+  {
+    "revision": "6f088317a4a1503fb0279f1c40eecb26",
+    "url": "/Blog-App/static/media/twit.6f088317.png"
+  },
+  {
+    "revision": "b9e5d3bd3eca3bcc8a67907d7ae42b2e",
+    "url": "/Blog-App/static/media/twitch.b9e5d3bd.png"
+  },
+  {
+    "revision": "62f278a28cc898e32086fb77f622df90",
+    "url": "/Blog-App/static/media/youtube.62f278a2.png"
+  }
+]);
